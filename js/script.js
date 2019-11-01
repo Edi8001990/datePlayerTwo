83e26156ca7751d94949f297198cd2d65fdc615f
Your Name <you@example.com>
2019-11-01 13:35:39 +0000

    Jquery done

---
+++ js/script.js
@@ -0,0 +1,15 @@
+
+$( document ).ready(function() {
+$(".link").click(function(e){
+    e.preventDefault();
+        $('.forms-container #link1').hide();
+        $('.forms-container #link2').hide();
+        $('#' + $(this).data('rel')).show();
+        
+});
+});
+
+
+
+
+
