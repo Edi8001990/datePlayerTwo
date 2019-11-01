1310e6eeb298c130eaa6957d2fe41ebba7f8444a
Your Name <you@example.com>
2019-11-01 21:51:09 +0000

    Nav works

--- js/script.js
+++ js/script.js
@@ -1,21 +1,13 @@
 
 $( document ).ready(function() {
-$(".link").click(function(e){
-    e.preventDefault();
-        $('.forms-container #link1').hide();
-        $('.forms-container #link2').hide();
-        $('#' + $(this).data('rel')).show();
-        
-});
 
-
-$('.inline-block #change-bg').click(function(){
-    $('#change-bg.red').removeClass('red')
-        $(this).addClass('red');
-});
-
-
-
+        $(".link").click(function(e){
+            e.preventDefault();
+                $('.forms-container #link1').hide();
+                $('.forms-container #link2').hide();
+                $('#' + $(this).data('rel')).show();
+                
+        });
 });
 
 
